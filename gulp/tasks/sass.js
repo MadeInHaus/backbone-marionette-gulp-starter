c661eb0f2a2cca4bd979fda7b5ae1a74e95593f8
Steve Scavo <stevescavo@gmail.com>
2016-04-27 16:28:22 -0700

    Write sourcemaps after prefixing

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -6,15 +6,15 @@ var handleErrors = require('../util/handleErrors');
 var config = require('../config').sass;
 var autoprefixer = require('gulp-autoprefixer');
 
-var taskDef = function () {
+var taskDef = function() {
     return gulp.src(config.src)
         .pipe(sourcemaps.init())
         .pipe(sass(config.settings))
         .on('error', handleErrors)
-        .pipe(sourcemaps.write())
         .pipe(autoprefixer({
             browsers: ['last 2 versions']
         }))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest(config.dest))
         .pipe(browserSync.stream());
 };
