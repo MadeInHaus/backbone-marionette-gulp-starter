3fc9f60d6b67b8c264f2a3615e9a2c4afcaa115a
Steve Scavo <stevescavo@gmail.com>
2016-04-27 16:25:22 -0700

    Swap `gulp-cssnano` for `gulp-clean-css`

--- gulp/tasks/minifyCss.js
+++ gulp/tasks/minifyCss.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp');
 var config = require('../config').production;
-var cssnano = require('gulp-cssnano');
+var cleanCSS = require('gulp-clean-css');
 var size = require('gulp-filesize');
 
 gulp.task('minifyCss', ['sass'], function() {
     return gulp.src(config.cssSrc)
-        .pipe(cssnano())
+        .pipe(cleanCSS())
         .pipe(gulp.dest(config.dest + '/css'))
         .pipe(size());
 });
